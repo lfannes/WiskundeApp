d3b64003acf4f9667e60f3eb18fe13b7531245e7
Annemie Janssens <annemie.fannes@gmail.com>
2016-03-31 11:33:36 +0200

    je kan nu zien hoeveel juiste antwoorden je gegeven hebt en hoeveel foute.

--- WiskundeApp.js
+++ WiskundeApp.js
@@ -8,32 +8,44 @@ function maakRandomGetalTussen0En(max)
 if (Meteor.isClient) {
   Session.setDefault('term1', maakRandomGetalTussen0En(10));
   Session.setDefault('term2', maakRandomGetalTussen0En(10));
+  Session.setDefault('juisteScore', 0);
+  Session.setDefault('fouteScore', 0);
 
   Template.som.helpers({
     term1: function () {
       return Session.get('term1');
     },
-   term2: function () {
+    term2: function () {
       return Session.get('term2');
-    } 
+    }
   });
-
+  Template.score.helpers({
+    juisteScore: function () {
+      return Session.get('juisteScore');
+    },
+    fouteScore: function () {
+      return Session.get('fouteScore');
+    }
+  });
   Template.som.events({
     'submit .antwoord-formulier': function (event) {
+      event.preventDefault();
       var juistAntwoord = Session.get('term1')+ Session.get('term2');
       var userAntwoord = Number(event.target.antwoord.value);
       if (userAntwoord == juistAntwoord)
       {
+        Session.set('juisteScore', Session.get('juisteScore')+1);
         alert('Je hebt het goed gedaan!!!');
       }
       else
       {
+        Session.set('fouteScore', Session.get('fouteScore')+1);
         alert('Sorry, je hebt het mis. Het was ' +juistAntwoord);
       }
 
       Session.set('term1', maakRandomGetalTussen0En(10));
       Session.set('term2', maakRandomGetalTussen0En(10));
-     },
+    }
   });
 }
 
